e8be73ddd3a7a1bfa6f3987b9795c1f4cca3db16
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-10-19 01:31:58 +0530

    riju

--- index.js
+++ index.js
@@ -12,16 +12,30 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.DATABASE_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch(() => {
-    console.log("Failed to connect to MongoDB");
-  });
+// mongoose
+//   .connect(process.env.DATABASE_URL)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch(() => {
+//     console.log("Failed to connect to MongoDB");
+//   });
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
 app.use("/api", router);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+connectDB();
